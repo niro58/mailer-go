88de1836c2703417988adfd6ca4187ad4d367cc5
niro58 <nichita.roilean@gmail.com>
2025-08-13 13:37:58 +0200

    template config fix

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	env "mailer-go/internal/environment"
 	handler "mailer-go/internal/handlers"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +11,7 @@ import (
 func AuthRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		auth := c.GetHeader("Authorization")
-
+		auth = strings.ReplaceAll(auth, "Bearer ", "")
 		if auth != env.Env.ApiAuth {
 			handler.Respond(c, nil, handler.ErrUnauthorized)
 			c.Abort()
